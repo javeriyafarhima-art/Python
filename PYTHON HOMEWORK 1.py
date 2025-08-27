6ecb697c66ec4fcbb1a8928f3c0052f31cd1e2a9
javeriyafarhima-art <javeriyafarhima@gmail.com>
2025-08-27 11:06:29 +0530

    Add files via upload

---
+++ PYTHON HOMEWORK 1.py
@@ -0,0 +1,15 @@
+x="coNgrAtuLaTioNS"
+print(str.lower(x))
+
+print(str.upper(x))
+
+print("Enter the First String:")
+str0ne=input()
+
+print("Enter the Second String:")
+strTwo=input()
+
+# Concatenate the two strings
+#Using Addition Operator
+strThree = strone+" "+ strTwo
+print("\nConcatenated String: ", strThree)
