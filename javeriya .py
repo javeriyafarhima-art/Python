9d01bbcd84141cb1107bf9807a080e634b65298c
javeriyafarhima-art <javeriyafarhima@gmail.com>
2025-09-08 16:32:39 +0530

    Add files via upload

---
+++ javeriya .py
@@ -0,0 +1,15 @@
+rno=[]
+mks=[]
+for a in range(4):
+    r,m=eval(input("Enter Roll No.,Marks:"))
+    rno.append(r)
+    mks.append(m)
+d={rno[0]:mks[0],rno[1]:mks[1],rno[2]:mks[2],rno[3]:mks[3]}
+print("Created dictionary")
+print(d) 
+
+       :     # code of program 1
+    if d [2]>75:
+        print("Roll number 2 scored",d[2],"(>75)")
+    else:
+        print("Roll number 2 scorced",d[2],"(>75)") 
