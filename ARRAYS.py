0cda2970ab0bcf3b891448bc594650a32e15368e
javeriyafarhima-art <javeriyafarhima@gmail.com>
2025-09-24 19:53:58 +0530

    Add files via upload

---
+++ ARRAYS.py
@@ -0,0 +1,7 @@
+import array as arr
+array_num = arr.array('i', [1,3,5,7,9,13])
+print("Original array: "+str(array_num))
+print("Number of occurences of the number 3 in the said array: "+str(array_num.count(3)))
+array_nr.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))      
