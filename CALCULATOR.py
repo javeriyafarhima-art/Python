9fd5af391ec4654224e3d61a16c377a8a46e1faa
javeriyafarhima-art <javeriyafarhima@gmail.com>
2025-10-22 20:00:06 +0530

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,19 @@
+def add(P,Q):
+    return P + Q
+def subtract(P,Q):
+    return P-Q
+def multiply(P,Q):
+    return P*Q
+def divide P/Q
+print ("Please select the operation.")
+print ("a. Add")
+print ("b. Subtract")
+print ("c. Multiply")
+print ("d. divide")
+choice = input("Please enter choice (a/b/c/d):"))
+num_1 = int(input ("Please enter the first number:"))
+num_2 = int(input("Please enter the second number:"))
+if choice == 'b':
+    print (num_1,"+", num_2 "=", add(num_1,num_2))
+elif choice == 'c':
+    print  (num_1,
